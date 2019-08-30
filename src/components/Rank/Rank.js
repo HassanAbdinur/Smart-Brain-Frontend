89fafe2db4e286840e9da1a22604ddd9e59dacdc
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-29 19:00:45 -0700

    new repo for final results

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Welcome to the Image Recognition platform for facial recognition using'}
+            </div>
+            <div className='white f1'>
+                {'Clarifai API'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
