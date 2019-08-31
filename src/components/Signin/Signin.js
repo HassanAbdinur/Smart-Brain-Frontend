0c3eda05ed3a783c3129190831f7342f5629358b
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 16:40:34 -0700

    adding events on email signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 
     onSubmitSignIn = () => {
         console.log(this.state);
-        () => onRouteChange('home')
+        this.props.onRouteChange('home')
     }
 
     render() {
@@ -31,11 +31,23 @@ class Signin extends React.Component {
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
                             <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                            <input
+                            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="email" 
+                            name="email-address"  
+                            id="email-address" 
+                            onChange={this.onEmailChange}
+                        />
                         </div>
                         <div className="mv3">
                             <label className="db fw6 lh-copy f6" htmlFor="password" />Password<label />
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                            <input 
+                            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            type="password" 
+                            name="password"  
+                            id="password"
+                            onChange={}
+                        />
                         </div>
                     </fieldset>
                         <div className="">
