87734c9f7bdd0398f6775399dcc6b310d543f4cd
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 15:59:29 -0700

    adding some changes

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Signin extends React.Component {
     render() {
+        const { onRouteChange } = this.props;
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
@@ -19,14 +20,14 @@ class Signin extends React.Component {
                     </fieldset>
                         <div className="">
                             <input 
-                            onClick={() => this.props.onRouteChange('home')}
+                            onClick={() => onRouteChange('home')}
                             className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
                             type="submit" 
                             value="Sign In" 
                             />
                         </div>
                         <div className="lh-copy mt3">
-                            <p onClick={() => this.props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
                         </div>
                     </div>
                 </main>
