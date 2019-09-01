c115f66d4f730a46b346dc2a9166f7de956b9ec4
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 19:34:58 -0700

    loading the user

--- src/App.js
+++ src/App.js
@@ -39,12 +39,21 @@ class App extends Component {
       user: {
         id: '',
         name: '',
-        password: '',
         email: '',
         entries: 0,
         joined: ''
       }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState(user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    })
   }
 
   displayFaceBox = (box) => {
