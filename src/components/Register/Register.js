8868aad7a1324c35766a830e8ab56b341fb5ff54
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 17:39:14 -0700

    implementing name change events

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            name: ''
+        }
+    }
+    onNameChange = (event) => {
+        this.setState({name: event.target.value})
+    }
     render() {
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
