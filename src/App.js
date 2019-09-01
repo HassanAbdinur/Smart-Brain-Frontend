24dd452772a3c1dff873426caa7f663589efbd71
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 19:36:21 -0700

    worked on a little bug

--- src/App.js
+++ src/App.js
@@ -47,13 +47,13 @@ class App extends Component {
   }
 
   loadUser = (data) => {
-    this.setState(user: {
+    this.setState({user: {
       id: data.id,
       name: data.name,
       email: data.email,
       entries: data.entries,
       joined: data.joined
-    })
+    }})
   }
 
   displayFaceBox = (box) => {
