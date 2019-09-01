41534f1d628682cd1468305b8e46324fce049327
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-31 17:41:58 -0700

    implementing password change events

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -16,6 +16,10 @@ class Register extends React.Component {
     onEmailChange = (event) => {
         this.setState({email: event.target.value})
     }
+
+    onPasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
     render() {
         return (
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
