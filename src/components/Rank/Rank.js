be7c478d6459cc1818195ddd403fa04b588ff01c
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-01 19:32:22 -0700

    making changes to home page

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ name, entries }) => {
     return (
         <div>
             <div className='white f3'>
-                {'Welcome to the Image Recognition platform for facial recognition using'}
+                {`${name}, your current entry count is...`}
             </div>
             <div className='white f1'>
-                {'Clarifai API'}
+                {entries}
             </div>
         </div>
     );
